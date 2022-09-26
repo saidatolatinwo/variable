5d4a1035988cdf38e960c856bae5fc29c113df20
saidatolatinwo <saidatolatinwo2020@gmail.com>
2022-09-26 12:36:57 +0100

    created the html file

---
+++ Variable.js
@@ -0,0 +1,20 @@
+//calculate the circumference of circle and area of circle.
+//formula : Circumference of circle =(22/7) , (3.142)
+//          Area of circle = (7/22) , (3.142)
+radius=25;
+Pi=3.142;
+console.log("The circumference of circle is "+ (2*Pi)*radius);
+console.log("The area of circle is ",(Pi*(radius*radius)));
+
+let tempincels = 40;
+let tempinfahr = 1.8 * tempincels + 32;
+console.log(`${tempincels}c = ${tempinfahr}`);
+
+let tempinFahr = 20;
+let tempinCels = 1.8 * tempinFahr - 32;
+console.log(`${tempinfahr}F = ${tempinCels}`);
+
+const birthYear = 2000;
+const futureYear = 2026;
+let Age = futureYear - birthYear;
+console.log(`my future age will be ${Age} or ${Age-1}`);
